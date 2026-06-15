031847ea3f384cfba6d08b416b590170b267ada0
Rafazg <claudenirorafaelbjj@gmail.com>
2026-06-15 19:31:28 -0300

    Excluido arquivos não utilizados

--- scenes/jogador/jogador.gd
+++ scenes/jogador/jogador.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
+var velocidade_do_jogador: float = 400.0;
+var direcao_do_movimento: Vector2 = Vector2(0, 0);
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
-	pass
+	movimentar_jogador();
+	
+	
+
+
+func movimentar_jogador() -> void:
+	if Input.is_action_pressed("move_right"):
+		direcao_do_movimento.x = 1;
+	elif Input.is_action_pressed("move_left"):
+		direcao_do_movimento.x = -1;
+	else:
+		direcao_do_movimento.x = 0
+	
+	if Input.is_action_pressed("move_up"):
+		direcao_do_movimento.y = -1
+	elif Input.is_action_pressed("move_down"):
+		direcao_do_movimento.y = 1
+	else:
+		direcao_do_movimento.y = 0
+	
+	velocity = direcao_do_movimento.normalized() * velocidade_do_jogador
+	move_and_slide();
